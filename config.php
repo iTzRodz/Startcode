afa29baa0fb7a5144566ca4fddfb14ef52ff677c
Rodolfo <rndcsjj@gmail.com>
2022-01-19 11:44:58 -0300

    Melhorando a parte do front

--- config.php
+++ config.php
@@ -1,3 +1,4 @@
+
 <?php
     $dbHost = 'Localhost';
     $dbUsername = 'root';
--- paginas/cadastro.php
+++ paginas/cadastro.php
@@ -13,3 +13,16 @@
         $result = mysqli_query($conexao, "INSERT INTO Dado_pessoal(email, nome, data_nasc, cpf, sexo, telefone) VALUES('$email', '$nome', '$nascimento', '$cpf', '$sexo', '$telefone')");
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Obrigado</p>
+</body>
+</html>
